21ac64c6741a7f1128bf6275dda02d593f475183
Elena <0koshko0@gmail.com>
2024-03-05 23:44:13 +0300

    first

---
+++ src/test/java/ru/java/SpringEureka/SpringEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.SpringEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
